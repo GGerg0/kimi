d1ea7f124c5bc80b5fe557a2f9b4ea94a747ece1
GGergo <gecsegergo006@gmail.com>
2024-04-19 11:21:48 +0200

    3. feladat

--- kimi.py
+++ kimi.py
@@ -13,7 +13,8 @@ class Verseny:
 f = open("kimi.csv","rt",encoding="utf-8")
 adat = []
 for sor in f:
-    sor  = sor.strip().split()
+    sor  = sor.strip().split(";")
     adat.append(Verseny(sor[0],sor[1],sor[2],sor[3],sor[4],sor[5],sor[6],sor[7],sor[8],))
-    
+
+print(f"3. feladat: {len(adat)-1}")
         
