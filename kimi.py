51d400e69f9aa05bfdda5d8e719e1fab742a8bc9
GGergo <gecsegergo006@gmail.com>
2024-04-19 11:18:16 +0200

    1. feladat

---
+++ kimi.py
@@ -0,0 +1,19 @@
+class Verseny:
+    def __init__(self,datum,nagydij,helyezes,befejezett_kor,pont,csapat,celbaert,hatrany,hibaoka):
+        self.datum = datum
+        self.nagydij=nagydij
+        self.helyezes=helyezes
+        self.befejezett_kor=befejezett_kor
+        self.pont=pont
+        self.csapat=csapat
+        self.celbaert=celbaert
+        self.hatrany=hatrany
+        self.hibaoka=hibaoka
+
+f = open("kimi.csv","rt",encoding="utf-8")
+adat = []
+for sor in f:
+    sor  = sor.strip().split()
+    adat.append(Verseny(sor[0],sor[1],sor[2],sor[3],sor[4],sor[5],sor[6],sor[7],sor[8],))
+
+        
