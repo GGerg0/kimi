4ea7b3f3032dce020b2d502d9f3e1a8f5262103d
GGergo <gecsegergo006@gmail.com>
2024-04-19 11:35:41 +0200

    4. feladat hiányos

--- kimi.py
+++ kimi.py
@@ -14,7 +14,18 @@ f = open("kimi.csv","rt",encoding="utf-8")
 adat = []
 for sor in f:
     sor  = sor.strip().split(";")
-    adat.append(Verseny(sor[0],sor[1],sor[2],sor[3],sor[4],sor[5],sor[6],sor[7],sor[8],))
+    adat.append(Verseny(sor[0],sor[1],sor[2],sor[3],sor[4],sor[5],sor[6],sor[7],sor[8]))
 
 print(f"3. feladat: {len(adat)-1}")
+
+
+
+for verseny in adat:
+    print(verseny)
+    if verseny.nagydij == "Magyar Nagydíj":
+        if verseny == "I":
+            print(f"{verseny.datum} : {verseny.helyezes}. hely")
+
+
+hiba = {}
         
